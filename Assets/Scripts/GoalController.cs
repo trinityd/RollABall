a7e99f690ac6c98bfe047310a9fdc971eb04af43
Friend Trinity Dickinson <ftrinitydickinson@gmail.com>
2018-09-22 03:54:21 -0700

    First commit

---
+++ Assets/Scripts/GoalController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour {
+
+    public float xRotationSpeed;
+    public float yRotationSpeed;
+    public float zRotationSpeed;
+
+    private Vector3 spin;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        spin = new Vector3(xRotationSpeed, yRotationSpeed, zRotationSpeed);
+        transform.Rotate(spin * Time.deltaTime);
+	}
+}
